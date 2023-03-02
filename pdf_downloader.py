50bc82a0f02cd0defc9cce9fd947bca07d47e5bc
10188 <1018876625@qq.com>
2023-03-03 00:11:31 +0800

    push

--- pdf_downloader.py
+++ pdf_downloader.py
@@ -12,7 +12,24 @@ class SciHub:
     def download(self, doi):
         data = {'request': doi}
         responses = requests.post(data=data, url=self.root_net, headers=self.headers).text
-        print(responses)
+        title = self._get_title(responses)
+        pdf = self._get_pdf_url(responses)
+        # 发起请求
+        binary = requests.get(url=pdf, headers=self.headers).content
+        # 存储pdf
+        with open(title+'.pdf', 'wb') as F:
+            F.write(binary)
+
+    def _get_title(self, html):
+        bs = BeautifulSoup(html, 'lxml')
+        title = bs.find(name='title').text.split('|')[1]
+        return title
+
+    def _get_pdf_url(self, html):
+        bs = BeautifulSoup(html, 'lxml')
+        pdf = bs.find(name='div', attrs={'id': 'article'}).find(name='iframe').get('src')
+        pdf_url = pdf.split('#')[0]
+        return pdf_url
 
 sh = SciHub()
 sh.download(doi='https://doi.org/10.1016/j.eswa.2020.113973')
